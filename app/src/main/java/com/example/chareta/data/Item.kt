2e4be3c681c4c800a238cb9be4d0f32a01e6de40
unknown <bereketheramo25@gmail.com>
2019-06-06 19:36:55 +0300

    Item data class added

---
+++ app/src/main/java/com/example/chareta/data/Item.kt
@@ -0,0 +1,12 @@
+package com.example.chareta.data
+
+import java.util.*
+
+data class Item (
+    private val id: Long,
+    private val item_name: String,
+    private val item_description: String,
+    private val starting_price: Long,
+    private val post_date: Date,
+    private val expiry_date: Date
+)
